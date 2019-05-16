3fd1bd4fc96e4c42e204dfe17bba3c97e300e39f
dsshli <dsshli@163.com>
2019-05-16 09:56:05 +0800

    添加log模块实现

--- src/com/shli/logs/LogService.java
+++ src/com/shli/logs/LogService.java
@@ -1,4 +1,13 @@
 package com.shli.logs;
 
 public class LogService {
+
+    /**
+     * 通过id获取消息体
+     * @param id
+     * @return
+     */
+    public String queryLogById(String id){
+        return "";
+    }
 }
